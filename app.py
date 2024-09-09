d43351b0eb26e01d1d12bec1a07d12e8b860cffd
Junior <jhurtado@plannerapp.net>
2024-09-09 14:02:08 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+# from flask import Flask
+# app = Flask (__name__)
+
+# @app.route('/')
+# def principal(): return "Bienvenidos o bienvenida a mi sitio web con PYTHON"
+# @app.route('/contacto')
+# def contacto(): return "Esta es la página de contactos HOY"
+# @app.route('/lenguajeprogramacion')
+# def lenguajes(): return "Esta es la pagina de lenguajes de programación"
+
+# if __name__ == '__main__': app.run(debug=True, port= 8000)
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    user = {'username': 'Juan'}
+    return render_template('index.html', title='Home', user=user)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/index.js
@@ -0,0 +1,5 @@
+import 'datatables.net-dt';
+ 
+let table = new DataTable('#myTable', {
+    // config options...
+});
